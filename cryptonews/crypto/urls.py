a78bebaff4bdca075e24a2c138f8bfd9682d4e5b
Burak Yılmaz <burakk.ylmaz@outlook.com>
2020-07-07 18:31:32 +0300

    start project crypto news

--- cryptonews/crypto/urls.py
+++ cryptonews/crypto/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('prices/', views.prices, name="prices"),
+    path('searchprices/', views.prices, name="searchprice"),
 ]
