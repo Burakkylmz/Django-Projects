909ac8d2b8ef8ed9e5090de2acac664b6d593fa6
Burak Yılmaz <burakk.ylmaz@outlook.com>
2020-07-07 18:28:39 +0300

    start project crypto news

--- cryptonews/crypto/views.py
+++ cryptonews/crypto/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+import requests
+import json
+
+def home(request):
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+    return render(request, 'home.html', {'api': api})
+
+def prices(request):
+    price_request = requests.get(
+        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,XLM,ADA,USDT,MIOTA,TRX&tsyms=USD")
+    price = json.loads(price_request.content)
+    return render(request, 'prices.html', {'price': price})
+
+def searchprices(request):
+    if request.method == 'POST':
+        quote = request.POST['quote']
+        quote = quote.upper()
+        crypto_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=USD")
+        crypto = json.loads(crypto_request.content)
+        return render(request, 'searchprice.html', {'quote':quote, 'crypto': crypto})
+    else:
+        notfound = "Enter a crypto currency symbol into the form above..."
+        return render(request, 'prices.html', {'notfound': notfound})
